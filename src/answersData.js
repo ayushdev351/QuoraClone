8b1316b67a2f518a3906db5c31a0d4adb36b4e82
ayushdev351 <79743704+ayushdev351@users.noreply.github.com>
2021-06-19 18:09:24 +0530

    Add Comment in answerData

--- src/answersData.js
+++ src/answersData.js
@@ -8,7 +8,8 @@ const answersData = [
       "Google itself indicates the most important factors of the page quality.Most probably the result will be declared on or before 18th of June 2021. But as you know with AKTU nothing can be said with hundered percent surity.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     views: 500,
     upvotes: 55,
-    downvotes: 3
+    downvotes: 3,
+    comments: 5,
   },
   {
     _id: "2",
@@ -19,7 +20,8 @@ const answersData = [
       "Most probably the result will be declared on or before 18th of June 2021. But as you know eith AKTU nothing can be said with hundered percent surity.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     views: 500,
     upvotes: 55,
-    downvotes: 3
+    downvotes: 3,
+    comments: 6,
   },
   {
     _id: "3",
@@ -31,7 +33,8 @@ const answersData = [
       "Google itself indicates the most important factors of the page quality. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     views: 500,
     upvotes: 55,
-    downvotes: 3
+    downvotes: 3,
+    comments:10,
   },
   {
     _id: "4",
@@ -43,7 +46,8 @@ const answersData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Google itself indicates the most important factors of the page quality.",
     views: 500,
     upvotes: 55,
-    downvotes: 3
+    downvotes: 3,
+    comments: 2,
   }
 ];
 
